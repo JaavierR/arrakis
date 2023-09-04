01acec350a9d1fad37daf00360f894bc73a22cf5
Javier Reyes <javier.reys@gmail.com>
2023-09-04 11:05:04 -0300

    fix: remove experimental flag on astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
